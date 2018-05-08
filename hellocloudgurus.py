2324511211bbba1b7018f68faec2b3798577b2a0
shreeshsp <38979825+shreeshsp@users.noreply.github.com>
2018-05-08 23:51:14 +0530

    Add files via upload

---
+++ hellocloudgurus.py
@@ -0,0 +1,12 @@
+def lambda_handler(event, context):
+    print("In lambda handler")
+    
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "Shreesh S P"
+    }
+    
+    return resp
